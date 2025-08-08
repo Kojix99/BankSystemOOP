674cd6a06d0d94df38baf17778af4e30b8507945
Sohail Zoraibi <sohailxzoraibi@gmail.com>
2025-08-08 14:43:00 +0100

    Update BankSystemOOP.cpp

--- BankSystemOOP.cpp
+++ BankSystemOOP.cpp
@@ -1,3 +1,13 @@
+/********************************************************************
+ * Project: Banking Management System
+ * Author : Sohail Zoraibi
+ * Date   : June 27, 2025
+ * Description:
+ *   A simple console-based banking system in C++ that manages users,
+ *   clients, permissions, and transactions with file storage.
+ ********************************************************************/
+
+
 #include <iostream>
 
 #include "clsLoginScreen.h"
